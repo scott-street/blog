d3085a7e065d92fca52ad49191dc24b46ca64d72
Kate Brune <kbrune@iastate.edu>
2021-02-06 17:35:05 -0600

    Deploy website - based on 4f49a37c408b6ea0147dc04c8d3c73dbddfe599e

---
+++ 4fdb24b0.f66b675c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{70:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"world","name":"world","count":1,"permalink":"/blog/tags/world"}')}}]);
---
+++ d8ffc0ea.43402e09.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{78:function(l){l.exports=JSON.parse('{"title":"all posts","items":[{"title":"hello, world!","permalink":"/blog/hello-world"}]}')}}]);
